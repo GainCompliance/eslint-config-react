d2f3e017ab9e91c3194f635e7f33c469d20cc614
Travis Peterson <contact@travispeterson.us>
2021-01-14 12:24:37 -0600

    fix: disables forbid-prop-types rule

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ module.exports = {
         'import/no-extraneous-dependencies': ['error', {devDependencies: true}],
         'react/prop-types': 'off',
         'react/jsx-props-no-spreading': 'off',
+        'react/forbid-prop-types': 'off',
         'react/jsx-boolean-value': ['error', 'always'],
         'jsx-a11y/click-events-have-key-events': 'off',
         'jsx-a11y/no-static-element-interactions': 'off'
